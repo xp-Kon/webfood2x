0fabcb5ce5188f3dec7e402b71af15833be673ec
xp-Kon <85104992+xp-Kon@users.noreply.github.com>
2025-03-26 15:02:52 +0000

    CVB C #	modified:   models.py

--- models.py
+++ models.py
@@ -6,4 +6,9 @@ class MenuItem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     image_data = db.Column(db.LargeBinary, nullable=True)  # 存储图片的二进制数据
-    image_mimetype = db.Column(db.String(50_
+    image_mimetype = db.Column(db.String(50), nullable=True)  # 存储图片类型 (MIME Type)
+
+    def __init__(self, name, image_data=None, image_mimetype=None):
+        self.name = name
+        self.image_data = image_data
+        self.image_mimetype = image_mimetype
